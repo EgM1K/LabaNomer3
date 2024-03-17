e8cf64df25c70f5eeb4c084e3e6953c92f77e7e5
EgM1k <23fmif.ye.mykhailenko@std.udu.edu.ua>
2024-03-17 22:56:15 +0200

    Написав основу класу ордер

--- LabaNomer3/Order.cs
+++ LabaNomer3/Order.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace LabaNomer3
 {
-    internal class Order
+    public class Order
     {
+        public List<Dish> Dishes { get; set; }
+        public double TotalAmount { get; set; }
+        public string Status { get; set; }
+        public Restaurant Restaurant { get; set; }
+        public Courier Courier { get; set; }
+        public Client Client { get; set; }
+
+        public Order(List<Dish> dishes, Restaurant restaurant, Client client)
+        {
+            Dishes = dishes;
+            TotalAmount = CalculateTotalAmount();
+            Status = "Виконано";
+            Restaurant = restaurant;
+            Client = client;
+        }
+
+        private double CalculateTotalAmount()
+        {
+            double total = 0;
+            foreach (var dish in Dishes)
+            {
+                total += dish.Price;
+            }
+            return total;
+        }
+
+        public void UpdateStatus(string status)
+        {
+            Status = status;
+        }
     }
 }
