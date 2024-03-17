d6e3bd0e5fe99f70b9ed71b8c524a0b844002547
EgM1k <23fmif.ye.mykhailenko@std.udu.edu.ua>
2024-03-17 22:38:33 +0200

    Зробив основу, додав всі класи

---
+++ LabaNomer3/Dish.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabaNomer3
+{
+    internal class Dish
+    {
+    }
+}
