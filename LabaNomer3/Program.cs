8cf246ba6f989e05992ce14208677c1d8d1a8009
EgM1k <23fmif.ye.mykhailenko@std.udu.edu.ua>
2024-03-17 22:32:07 +0200

    Добавьте файлы проекта.

---
+++ LabaNomer3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LabaNomer3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Тут має бути код :)");
+        }
+    }
+}
