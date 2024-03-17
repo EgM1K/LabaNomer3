53cf65d8d363a4b95179236db7dab49da83d8e5c
EgM1k <23fmif.ye.mykhailenko@std.udu.edu.ua>
2024-03-17 22:47:40 +0200

    Зробив основу для класу стриви без методів

--- LabaNomer3/Dish.cs
+++ LabaNomer3/Dish.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace LabaNomer3
 {
-    internal class Dish
+    public class Dish
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public Dish(string name, string descriotion, double price)
+        {
+            Name = name;
+            Description = descriotion;
+            Price = price;
+        }
     }
 }
