afbc206da23ff5cf5422bc2191d9e109fdf4d483
EgM1k <23fmif.ye.mykhailenko@std.udu.edu.ua>
2024-03-17 22:52:53 +0200

    Написав основу класу курєр без методів

--- LabaNomer3/Courier.cs
+++ LabaNomer3/Courier.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LabaNomer3
 {
-    internal class Courier
+    public class Courier
     {
+        public string Name { get; set; }
+        public string ContactInfo { get; set; }
+        public double Raiting { get; set; }
+        public string TransportType { get; set; }
+        public Courier(string name, string contactInfo, double raiting, string transportType) 
+        { 
+            Name = name;
+            ContactInfo = contactInfo;
+            Raiting = raiting;
+            TransportType = transportType;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Ім'я: {Name}, Контактна інформація: {ContactInfo}, Рейтинг: {Raiting}, Тип транспорту: {TransportType}");
+        }
     }
 }
