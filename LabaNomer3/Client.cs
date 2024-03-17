fe761b27811b90705c824c166616264b77391701
EgM1k <23fmif.ye.mykhailenko@std.udu.edu.ua>
2024-03-17 22:54:16 +0200

    Написав основу класу клиенту без методів

--- LabaNomer3/Client.cs
+++ LabaNomer3/Client.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace LabaNomer3
 {
-    internal class Client
+    public class Client
     {
+        public string Name { get; set; }
+        public string DeliveryAddress { get; set; }
+        public string ContactNumber { get; set; }
+        public List<Order> OrderHistory { get; set; }
+
+        public Client(string name, string deliveryAddress, string contactNumber)
+        {
+            Name = name;
+            DeliveryAddress = deliveryAddress;
+            ContactNumber = contactNumber;
+            OrderHistory = new List<Order>();
+        }
     }
 }
