8557a63c9c01d877ae28a2b83cc8e2039aed6ebf
EgM1k <23fmif.ye.mykhailenko@std.udu.edu.ua>
2024-03-17 22:44:57 +0200

    Зробив основу для класу ресторан

--- LabaNomer3/Restaurant.cs
+++ LabaNomer3/Restaurant.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace LabaNomer3
 {
-    internal class Restaurant
+    public class Restaurant
     {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Type { get; set; }
+        public double Raiting { get; set; }
+        public List<Dish> Dishes { get; set; }
+        public Restaurant(string name, string address, string type, double raiting, List<Dish> dishes) 
+        {
+            Name = name;
+            Address = address;
+            Type = type;
+            Raiting = raiting;
+            Dishes = dishes;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Ім'я: {Name}, Адрес: {Address}, Тип: {Type}, Рейтинг: {Raiting}");
+        }
     }
 }
